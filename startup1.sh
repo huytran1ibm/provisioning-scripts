75f8bf46ff72c163405c7cb908518b50927442e4
huytran1ibm <36869410+huytran1ibm@users.noreply.github.com>
2021-09-28 10:16:01 -0500

    Update startup1.sh

--- startup1.sh
+++ startup1.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-wget https://2142-2600-1700-5030-4e90-14f5-93c7-3425-ef6b.ngrok.io/hihuy/${HOST}
+wget https://2142-2600-1700-5030-4e90-14f5-93c7-3425-ef6b.ngrok.io/hihuy/$(date +"%T")
 
 mkdir /var/www
 touch /var/www/index.html
 echo "hi huy2" >> /var/www/index.html
+
+
+cat << EOF > busybox-httpd.sh
+#!/bin/sh
 busybox httpd -p 0.0.0.0:80 -h /var/www/ &
+EOF
+
+chmod +x busybox-httpd.sh
+mv busybox-httpd.sh /etc/init.d/
