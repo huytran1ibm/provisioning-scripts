5ad5c996e35424d1f5015a00bb9f0f144fea5b25
huytran1ibm <36869410+huytran1ibm@users.noreply.github.com>
2021-09-30 10:31:49 -0500

    Update startup1.sh

--- startup1.sh
+++ startup1.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 host=$(cat /etc/hostname)
 wget https://4849-2600-1700-5030-4e90-c4d4-3b79-e7c0-b7d8.ngrok.io/hihuy/${host}
+
+indexhtml=/var/www/html/index.nginx-debian.html
+
+# wait for the cloud-init boot process to complete
+until [ -f /$indexhtml ]; do
+  date
+  sleep 11
+done
+
+# initial value
+thishost=$(cat /etc/hostname)
+cat > $indexhtml <<EOF
+INIT - ${thishost}
+EOF
+
+# Internet is availble then more software can be installed from the internet like
+# npm, pip, docker, ...  if isolated only the software from the ibm mirrors can be installed
+if curl -o /tmp/x https://www.python.org/downloads/release/python-373/; then
+cat > $indexhtml <<EOF
+INTERNET - ${thishost}
+EOF
+else
+cat > $indexhtml <<EOF
+ISOLATED - ${thishost}
+EOF
+fi
